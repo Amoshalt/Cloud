7914e441eb1ec1ebfc0ba8ef47db274f6c6a7435
Martial Tardy <tardy.m02@gmail.com>
2018-12-12 00:02:04 +0100

    [userController] Javadoc + correction test path hello world

--- cloud/src/main/java/com/cloud/controller/UserController.java
+++ cloud/src/main/java/com/cloud/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cloud.controller;
 
+import com.cloud.models.User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.json.JsonObject;
@@ -8,37 +9,64 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/")
 public class UserController {
+
+
+    /** get all users
+     * @return
+     */
     @GetMapping("/users")
-    public JsonObject getUsers() {
+    public User[] getUsers() {
         return null;
     }
 
+    /** replace users by users
+     * @return
+     */
     @PutMapping("/users")
     public JsonObject putUsers() {
         return null;
     }
 
+    /** delete all users
+     * @return
+     */
     @DeleteMapping("/users")
     public JsonObject deleteUsers() {
         return null;
     }
 
 
+    /** get user by id
+     * @param id
+     * @return User
+     */
     @GetMapping("/user/{id}")
-    public JsonObject getUser(@PathVariable(value = "id") int id) {
+    public User getUser(@PathVariable(value = "id") int id) {
         return null;
     }
 
+
+    /** add user
+     * @return
+     */
     @PostMapping("/user")
     public JsonObject postUser() {
         return null;
     }
 
+    /** update user by id
+     * @param id
+     * @return
+     */
     @PutMapping("/user/{id}")
     public JsonObject putUser(@PathVariable(value = "id") int id) {
         return null;
     }
 
+    /** delete user by id
+     * @param id
+     * @return
+     */
     @DeleteMapping("/user/{id}")
     public JsonObject deleteUser(@PathVariable(value = "id") int id) {
         return null;
