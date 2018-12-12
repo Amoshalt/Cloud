26b81e85e60fc623a0770767f7786f796c5cca3a
Martial Tardy <tardy.m02@gmail.com>
2018-12-12 11:46:44 +0100

    users

--- src/main/java/com/cloud/controller/UserController.java
+++ src/main/java/com/cloud/controller/UserController.java
@@ -1,38 +1,49 @@
 package com.cloud.controller;
 
 import com.cloud.models.User;
+import com.cloud.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.json.JsonObject;
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
 
+    @Autowired
+    private UserRepository userRepository;
+
+
     /** get all users
      * @return
      */
-    @GetMapping("/users")
-    public User[] getUsers() {
-        return null;
+    @GetMapping("/user")
+    public List<User> getUsers() {
+        System.out.println("coucou");
+        return this.userRepository.findAll();
     }
 
     /** replace users by users
      * @return
      */
-    @PutMapping("/users")
-    public JsonObject putUsers() {
-        return null;
+    @PutMapping("/user")
+    public void putUsers(@Valid @RequestBody List<User> users) {
+        System.out.println(users);
+        this.userRepository.deleteAll();
+        this.userRepository.saveAll(users);
     }
 
     /** delete all users
      * @return
      */
-    @DeleteMapping("/users")
-    public JsonObject deleteUsers() {
-        return null;
+    @DeleteMapping("/user")
+    public void deleteUsers() {
+        this.userRepository.deleteAll();
     }
 
 
@@ -41,8 +52,8 @@ public class UserController {
      * @return User
      */
     @GetMapping("/user/{id}")
-    public User getUser(@PathVariable(value = "id") int id) {
-        return null;
+    public Optional<User> getUser(@PathVariable(value = "id") Long id) {
+        return this.userRepository.findById(id);
     }
 
 
@@ -50,8 +61,8 @@ public class UserController {
      * @return
      */
     @PostMapping("/user")
-    public JsonObject postUser() {
-        return null;
+    public @Valid User postUser(@Valid @RequestBody User user) {
+        return this.userRepository.save(user);
     }
 
     /** update user by id
@@ -59,7 +70,8 @@ public class UserController {
      * @return
      */
     @PutMapping("/user/{id}")
-    public JsonObject putUser(@PathVariable(value = "id") int id) {
+    public JsonObject putUser(@PathVariable(value = "id") int id, @Valid @RequestBody User user) {
+//        return this.userRepository.;
         return null;
     }
 
