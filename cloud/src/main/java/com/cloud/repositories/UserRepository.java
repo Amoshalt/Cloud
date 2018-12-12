ab95980419045380fc70f4151fbd29de5b42f267
Martial Tardy <tardy.m02@gmail.com>
2018-12-12 08:59:55 +0100

    [mongoDB] start repo

---
+++ cloud/src/main/java/com/cloud/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package com.cloud.repositories;
+import com.cloud.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, Long>{
+}
