6f4176868c3b4434e00146b877267b5f7fc89056
Victor REMOND <victor96@hotmail.fr>
2018-12-19 11:41:21 +0100

    Display env variables

--- src/main/java/com/cloud/controller/HelloController.java
+++ src/main/java/com/cloud/controller/HelloController.java
@@ -4,11 +4,26 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+import static java.lang.String.format;
+
 @RestController
 @RequestMapping("/")
 public class HelloController {
     @RequestMapping("/hello")
     public String home() {
         return "Hello World!";
+    }
+    @RequestMapping("/env")
+    public String env() {
+        Map<String, String> env = System.getenv();
+        StringBuilder builder = new StringBuilder();
+        for (String envName : env.keySet()) {
+            builder.append( format("%s=%s%n",
+                    envName,
+                    env.get(envName)));
+        }
+        return builder.toString();
     }
 }
