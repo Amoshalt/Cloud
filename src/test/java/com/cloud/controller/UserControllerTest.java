989fffe08a4ba066dc74d742e233d43fc664f833
Martial Tardy <tardy.m02@gmail.com>
2018-12-19 11:17:01 +0100

    [userTest] first config

--- src/test/java/com/cloud/controller/UserControllerTest.java
+++ src/test/java/com/cloud/controller/UserControllerTest.java
@@ -2,15 +2,26 @@ package com.cloud.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.Assert.*;
+import java.util.UUID;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
 public class UserControllerTest {
     
     @Autowired
@@ -29,6 +40,7 @@ public class UserControllerTest {
 
     @Test
     public void putUsers() {
+
     }
 
     @Test
@@ -48,14 +60,18 @@ public class UserControllerTest {
     }
 
     @Test
-    public void deleteUser() {
+    public void deleteUser() throws Exception {
+        UUID id = UUID.fromString("");
+        mockMvc.perform(MockMvcRequestBuilders.delete("/user/" + id))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Configuration
     public static class TestConfiguration {
 
         @Bean
-        public UserController helloController() {
+        public UserController userController() {
             return new UserController();
         }
 
