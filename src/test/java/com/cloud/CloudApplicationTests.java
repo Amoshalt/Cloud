919f1ceeee50e0453bacbc8a6032012b7e912a41
Elomidas <victor.remond.96@gmail.com>
2019-01-05 21:11:13 +0100

    Using embedded mongo for testing
    Should enable CircleCI to run test with mongo without error

--- src/test/java/com/cloud/CloudApplicationTests.java
+++ src/test/java/com/cloud/CloudApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,7 +19,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebAppConfiguration
 @AutoConfigureMockMvc
 @SpringBootTest
-@TestPropertySource(locations="classpath:test.properties")
 public class CloudApplicationTests{
 
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
