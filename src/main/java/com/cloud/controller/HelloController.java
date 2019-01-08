bcdca7d5b8ea1f6c2c4416a8f50a29cd238b568d
Unknown <lombardoflorian@gmail.com>
2019-01-08 18:37:09 +0100

    Ok

--- src/main/java/com/cloud/controller/HelloController.java
+++ src/main/java/com/cloud/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String home() {
-        return "Hello World                 !";
+        return "Hello World!";
     }
 }
